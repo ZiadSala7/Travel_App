3dcbcbe8009932a38561f67684f9adda8dc3797e
Ziad <zslah2800@gmail.com>
2025-11-06 14:02:52 +0200

    app_assets added

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for assetsImagesAuth
+  /// assets/images/auth.png
+  static const String assetsImagesAuth = "assets/images/auth.png";
+
+  /// Assets for assetsImagesCertification
+  /// assets/images/certification.png
+  static const String assetsImagesCertification = "assets/images/certification.png";
+
+  /// Assets for assetsImagesConfirmed
+  /// assets/images/confirmed.png
+  static const String assetsImagesConfirmed = "assets/images/confirmed.png";
+
+  /// Assets for assetsImagesForgPass
+  /// assets/images/forgPass.png
+  static const String assetsImagesForgPass = "assets/images/forgPass.png";
+
+  /// Assets for assetsImagesOrderConfirm
+  /// assets/images/orderConfirm.png
+  static const String assetsImagesOrderConfirm = "assets/images/orderConfirm.png";
+
+  /// Assets for assetsImagesProgramming
+  /// assets/images/programming.png
+  static const String assetsImagesProgramming = "assets/images/programming.png";
+
+  /// Assets for assetsImagesSuccess
+  /// assets/images/success.png
+  static const String assetsImagesSuccess = "assets/images/success.png";
+
+  /// Assets for assetsImagesUndrawCertificateCqps
+  /// assets/images/undraw_certificate_cqps.png
+  static const String assetsImagesUndrawCertificateCqps = "assets/images/undraw_certificate_cqps.png";
+
+  /// Assets for assetsImagesUndrawSignHereLxua
+  /// assets/images/undraw_sign-here_lxua.png
+  static const String assetsImagesUndrawSignHereLxua = "assets/images/undraw_sign-here_lxua.png";
+
+  /// Assets for assetsImagesWelcome
+  /// assets/images/welcome.png
+  static const String assetsImagesWelcome = "assets/images/welcome.png";
+
+  /// Assets for assetsImagesWellDone
+  /// assets/images/wellDone.png
+  static const String assetsImagesWellDone = "assets/images/wellDone.png";
+}
+
