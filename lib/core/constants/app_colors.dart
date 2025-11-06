36939f1163678e07344842ef213e7dc5548916fb
Ziad <zslah2800@gmail.com>
2025-11-06 13:39:35 +0200

    adding app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // ===== Primary Blues =====
+  static const Color primaryBlue = Color(0xFF1855AD);
+  static const Color deepBlue = Color(0xFF080E1E);
+  static const Color navyBlue = Color(0xFF222375);
+  static const Color oceanBlue = Color(0xFF141D3E);
+  static const Color skyBlue = Color(0xFF146ECF);
+  static const Color royalBlue = Color(0xFF232474);
+  static const Color brightBlue = Color(0xFF2960C1);
+  static const Color vividBlue = Color(0xFF3C69CE);
+  static const Color steelBlue = Color(0xFF5682C6);
+  static const Color softBlue = Color(0xFF4479D8);
+  static const Color calmBlue = Color(0xFF79AAEC);
+  static const Color aquaBlue = Color(0xFF13D5FF);
+  static const Color cloudBlue = Color(0xFF4F7FD3);
+  static const Color coolBlue = Color(0xFF5473A1);
+  static const Color iceBlue = Color(0xFF80A6E6);
+  static const Color paleBlue = Color(0xFFA6C2EA);
+  static const Color electricBlue = Color(0xFF1D8ADB);
+  static const Color neonBlue = Color(0xFF4CA0EE);
+  static const Color strongBlue = Color(0xFF3971D3);
+  static const Color slateBlue = Color(0xFF415A8E);
+  static const Color indigoBlue = Color(0xFF283E99);
+  static const Color deepIndigo = Color(0xFF172F7A);
+  static const Color boldBlue = Color(0xFF2C5EB5);
+  static const Color vibrantBlue = Color(0xFF0063BE);
+
+  // ===== Primary Yellows & Oranges =====
+  static const Color primaryYellow = Color(0xFFFFCE1F);
+  static const Color goldenYellow = Color(0xFFFFAF36);
+  static const Color brightYellow = Color(0xFFFFCB5D);
+  static const Color sunnyYellow = Color(0xFFFFB800);
+  static const Color softYellow = Color(0xFFF7FF98);
+  static const Color warmYellow = Color(0xFFFFAF39);
+  static const Color mellowYellow = Color(0xFFFFC774);
+  static const Color paleYellow = Color(0xFFFFE483);
+  static const Color citrusYellow = Color(0xFFFFB320);
+  static const Color amberYellow = Color(0xFFFFBD70);
+  static const Color vividYellow = Color(0xFFFFBC2D);
+  static const Color honeyYellow = Color(0xFFFFAD33);
+  static const Color goldenOrange = Color(0xFFFF9E0C);
+  static const Color deepOrange = Color(0xFFF94C00);
+  static const Color warmOrange = Color(0xFFEF9F27);
+  static const Color strongOrange = Color(0xFFFF910F);
+  static const Color tangerine = Color(0xFFFF9501);
+
+  // ===== Neutrals =====
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color offWhite = Color(0xFFF5F9FC);
+  static const Color creamWhite = Color(0xFFF7F6F0);
+  static const Color softGray = Color(0xFFC0D5F2);
+  static const Color lightGray = Color(0xFFD0E1F3);
+  static const Color paleGray = Color(0xFFCBCBCB);
+  static const Color mediumGray = Color(0xFF969490);
+  static const Color darkGray = Color(0xFF64646E);
+  static const Color charcoalGray = Color(0xFF32373F);
+  static const Color graphite = Color(0xFF2B292D);
+  static const Color deepCharcoal = Color(0xFF060913);
+
+  // ===== Accents & Others =====
+  static const Color purpleAccent = Color(0xFFB71EFF);
+  static const Color redAccent = Color(0xFFEA6F57);
+  static const Color bronze = Color(0xFFB76E00);
+}
